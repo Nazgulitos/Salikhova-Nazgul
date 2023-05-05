b25afd49e7819bc8ea66debcfca182a053bdbeca
Salikhova nazgul <109477146+Nazgulitos@users.noreply.github.com>
2023-05-05 17:40:25 +0300

    Update Task_1.cpp

--- Task_1.cpp
+++ Task_1.cpp
@@ -1,4 +1,5 @@
 // Salikhova Nazgul
+
 #include <iostream>
 #include <iomanip>
 #include <cmath>
@@ -8,54 +9,49 @@ using namespace std;
 #else
 #define GNUPLOT_NAME "gnuplot -persist"
 #endif
-int Task_1() {
-    #ifdef WIN32
-        FILE* pipe = _popen(GNUPLOT_NAME, "w");
-    #else
-        FILE* pipe = popen(GNUPLOT_NAME, "w");
-    #endif
+int main() {
+#ifdef WIN32
+    FILE* pipe = _popen(GNUPLOT_NAME, "w");
+#else
+    FILE* pipe = popen(GNUPLOT_NAME, "w");
+#endif
     cout<<fixed<<setprecision(2);
-    double V, K, N;
-    double T, alpha_1, beta_1, alpha_2, beta_2;
-    cin >> V >> K >> alpha_1 >> beta_1 >> alpha_2 >> beta_2 >> T >> N;
+    double V = 112.0;
+    double K = 45.0;
+    double alpha_1 = 0.5;
+    double beta_1 = 0.005;
+    double alpha_2 = 0.4;
+    double beta_2 = 0.006;
+    double T = 55.0;
+    double N = 200.0;
+
     double t = 0.00;
     double v0 = V - alpha_2/beta_2;
     double k0 = K - alpha_1/beta_1;
 
-    cout<<"t:"<<endl;
+    fprintf(pipe, "plot '-' using 1:2 with lines title 'victims',""'-' using 1:2 with lines title 'killers'\n");
     while (t <= T) {
-        cout<<t<<" ";
+        double x = alpha_2/beta_2 + v0*cos(sqrt(alpha_1*alpha_2) * t)
+                   - k0*(beta_1* sqrt(alpha_2))/(beta_2 * sqrt(alpha_1)) * sin(sqrt(alpha_1*alpha_2)*t);
+        fprintf(pipe, "%f\t%f\n", t, x);
         t += T/N;
     }
+
+    fprintf(pipe, "e\n");
     t = 0.00;
-    cout<<endl;
-    cout<<"v:"<<endl;
-    fprintf(pipe, "plot '-' using 1:2 with lines title 'victims',""'-' using 1:2 with lines title 'killers'\n");
-//    while (t <= T) {
-//
-//        fprintf(pipe, "%f\t%f\n", t, x);
-//        t += T/N;
-//    }
-//    fprintf(pipe, "e\n");
-    fprintf(pipe, "plot '-' using 1:2 with lines\n");
-    t = 0.00;
-    cout<<endl;
-    cout<<"k:"<<endl;
     while (t <= T) {
-        double victim = alpha_2/beta_2 + v0*cos(sqrt(alpha_1*alpha_2) * t)
-                   - k0*(beta_1* sqrt(alpha_2))/(beta_2 * sqrt(alpha_1)) * sin(sqrt(alpha_1*alpha_2)*t);
-        double killer = alpha_1/beta_1 + v0*(beta_2* sqrt(alpha_1))/(beta_1 * sqrt(alpha_2)) * sin(sqrt(alpha_1*alpha_2)*t)
-                  + k0*cos(sqrt(alpha_1*alpha_2) * t);
-        fprintf(pipe, "%f\t%f\n", killer, victim);
+        double y = alpha_1/beta_1 + v0*(beta_2* sqrt(alpha_1))/(beta_1 * sqrt(alpha_2)) * sin(sqrt(alpha_1*alpha_2)*t)
+                   + k0*cos(sqrt(alpha_1*alpha_2) * t);
+        fprintf(pipe, "%f\t%f\n", t, y);
         t += T/N;
     }
 
     fprintf(pipe, "e\n");
     fflush(pipe);
     return 0;
-    #ifdef WIN32
-        _pclose(pipe);
-    #else
-        pclose(pipe);
-    #endif
+#ifdef WIN32
+    _pclose(pipe);
+#else
+    pclose(pipe);
+#endif
 }
